24ead2ba3a212f50267f9acbd84e8563ab7ab680
Shivaraju NS <shivarajsmurthy@gmail.com>
2025-09-11 22:26:02 +0530

    Search algorithm in DSA

--- Searching/src/searching.java
+++ Searching/src/searching.java
@@ -1,30 +1,38 @@
 public class searching {
     public static void main(String[] args) {
-        int[] nums = {10,50,70,100,534};
-        int target = 534;
+        int[] nums = new int[10000000];
+        int target = 1000;
         int result=0;
 
         result=(binarysearch(nums, target));
-        System.out.println(result);
+        System.out.println("Element Found (Linear Search) at index: "+result);
         result=(linearsearch(nums, target));
-        System.out.println(result);
+        System.out.println("Element Found (Binary Search) at index: "+result);
 
     }
 
     public static int linearsearch(int[] nums, int target) {
+        int steps=0;
+
         for(int i=0; i<nums.length; i++){
+            steps++;
             if(nums[i] == target){
                 return i;
+
             }
         }
+        System.out.println("Number of Steps taken by LinerSearch is " + steps);
+
         return -1;
     }
 
     public static int binarysearch(int[] nums, int target){
         int left = 0;
         int right = nums.length -1;
+        int steps=0;
 
         while(left<=right){
+            steps++;
             int midValue = left + (right-left) /2;
 
             if(nums[midValue] == target){
@@ -38,7 +46,7 @@ public class searching {
                 right = midValue-1;
             }
         }
-
+        System.out.println("Number of Steps taken by BinarySearch is " + steps);
         return -1;
     }
 }
